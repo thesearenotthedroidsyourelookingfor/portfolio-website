7900fa53f0a61a68944c015e825db5593745a361
thesearenotthedroidsyourelookingfor <savannaclv1@gmail.com>
2025-01-24 19:54:14 -0800

    add js to make menu toggle

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
 console.log("Welcome to Savanna's Portfolio!")
+
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+        x.className += ' responsive';
+    } else {
+        x.className = 'navtoggle';
+    }
+}
